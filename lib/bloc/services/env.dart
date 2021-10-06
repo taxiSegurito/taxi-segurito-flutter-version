430de27778b0a87f8d6f620347c8c3541148ceb5
JoseeCarlos <montanoj47@gmail.com>
2021-10-06 01:12:59 -0400

    url xd

--- lib/bloc/services/env.dart
+++ lib/bloc/services/env.dart
@@ -1,5 +1,3 @@
 class Service {
-  // static const url = "https://taxi-segurito.000webhostapp.com/flutter_api/";
-
-  static const url = "https://10.0.2.2/flutter_api/";
+  static const url = "https://taxi-segurito.000webhostapp.com/flutter_api/";
 }
