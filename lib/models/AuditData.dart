bb7812862d847e740f4ce818adffcf4a041059e8
davidllanos09 <90053095+davidllanos09@users.noreply.github.com>
2021-10-17 15:15:40 -0400

    Delete AuditData.dart

--- lib/models/AuditData.dart
+++
@@ -1,9 +0,0 @@
-class AuditData {
-  late DateTime registerDate;
-  late DateTime updateDate;
-  late int status;
-
-  AuditData(this.registerDate, this.updateDate, this.status);
-
-  AuditData.Default();
-}
