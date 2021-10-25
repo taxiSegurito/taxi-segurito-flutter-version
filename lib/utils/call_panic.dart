856ee42c64139ea7fe66e77a2665ac0bfa69a50e
davidllanos0972 <lry0028267@est.univalle.edu>
2021-10-25 18:40:49 -0400

    Metodo para extraer Telefonos de BD

--- lib/utils/call_panic.dart
+++ lib/utils/call_panic.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
 import 'dart:developer';
 import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 import 'package:sms_advanced/sms_advanced.dart';
+import 'package:taxi_segurito_app/bloc/services/env.dart';
+import 'package:http/http.dart' as http;
+import 'package:taxi_segurito_app/models/sesions/sesion.dart';
 
 class CallPanic {
   Future<bool> Call_Number() async {
@@ -43,5 +47,32 @@ class CallPanic {
   List<String> GetListNumbers() {
     List<String> numbers = ["69685120", "69685120"];
     return numbers;
+  }
+
+  Future<List<String>> GetListFromDB() async {
+    Sessions sessions = Sessions();
+    List<String> numbers = List.empty();
+    bool googleSession = await sessions.verificationSession("emailGoogle");
+    bool facebookSession = await sessions.verificationSession("emailFacebook");
+    String idSession = "";
+    if (facebookSession == true) {
+      idSession = sessions.getSessionValue("idFacebook");
+    } else if (googleSession == true) {
+      idSession = sessions.getSessionValue("idGoogle");
+    }
+    try {
+      var url = Service.url + "UserAdd/UserController.php";
+      /*var url =
+          "http://192.168.0.3/backend-taxi-segurito-app/UserController.php";*/
+      var response = await http.post(Uri.parse(url),
+          body: jsonEncode(
+              {"idPerson": int.parse(idSession), "type": "GetListNumber"}));
+      var res = jsonDecode(response.body);
+      log(res['result'].toString());
+      return res['result'];
+    } catch (e) {
+      log(e.toString());
+      return List.empty();
+    }
   }
 }
