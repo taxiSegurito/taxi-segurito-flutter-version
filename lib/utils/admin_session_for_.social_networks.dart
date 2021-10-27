70453693b453984d7fff6e89859eae1572469a48
davidllanos0972 <lry0028267@est.univalle.edu>
2021-10-27 02:29:32 -0400

    Sessiones modificadas

--- lib/utils/admin_session_for_.social_networks.dart
+++ lib/utils/admin_session_for_.social_networks.dart
@@ -3,11 +3,22 @@ import 'package:taxi_segurito_app/models/sesions/sesion.dart';
 import 'package:taxi_segurito_app/utils/servces.dart';
 
 class AdminSession {
+  Sessions sessions = new Sessions();
   void AddSession(Clientuser client) async {
-    String IdGoogle = await Services().GetId(client.email);
-    Sessions sessions = new Sessions();
-    await sessions.addSessionValue("emailGoogle", client.email);
-    await sessions.addSessionValue("cellphoneGoogle", client.cellphone);
-    await sessions.addSessionValue("idGoogle", IdGoogle);
+    String Id = await Services().GetId(client.email);
+    await sessions.addSessionValue("iduser", Id);
+    await sessions.addSessionValue("rol", "Cliente");
+  }
+
+  Future<bool> DeleteSession() async {
+    bool idsession = await sessions.verificationSession("iduser");
+    bool rolsession = await sessions.verificationSession("rol");
+    if (idsession && rolsession) {
+      sessions.removeValuesSession("iduser");
+      sessions.removeValuesSession("rol");
+      return true;
+    } else {
+      return false;
+    }
   }
 }
