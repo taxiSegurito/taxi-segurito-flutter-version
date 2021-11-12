1151422af5b01892a792148f762138790a62b81f
LeoLeo48 <leonel.calderon.rivas@gmail.com>
2021-11-12 17:47:42 -0400

    Reworked crud, emergency_contact_service added

--- lib/pages/emergencyContact/contact_form_functionality.dart
+++ lib/pages/emergencyContact/contact_form_functionality.dart
@@ -1,152 +1,80 @@
-import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:taxi_segurito_app/models/emergencyContact.dart';
 import 'package:taxi_segurito_app/services/sessions_service.dart';
+import 'package:taxi_segurito_app/services/emergency_contact_service.dart';
 
-class FormContact_Functionality {
-  SessionsService sessions = SessionsService();
-  String idUser =
-      "0"; // Inicializa como 'invitado', con CheckID() se actualiza la sesion.
-  var dataSet;
-  //Link database
-  var db = "https://taxi-segurito.000webhostapp.com/flutter_api/";
+class FormContact_Functionality{
+
+  SessionsService sessions = SessionsService(); 
+  EmergencyContactService emergencyContactService = new EmergencyContactService();
+  bool isSession = false;
+  int sessionId = 0;
+
   //UI data controllers
   TextEditingController contactName_Controller = TextEditingController();
   TextEditingController contactNumber_Controller = TextEditingController();
 
-  Future CheckID() async {
-    bool idsession = await sessions.verificationSession("iduser");
-    if (idsession) {
-      idUser = sessions.getSessionValue("iduser").toString();
-      print("0: " + idUser);
-      return true;
-    } else
-      return false;
-  }
-  //Querys
-
-  // 1 GET query: Get data user at init widget
-  /*
-  Future GetContactData(String _id) async
+  CheckSession() async
   {
-    var contact;
-    var url = db+"getEmergencyContacts.php";
     try
     {
-      var response = await http.post(Uri.parse(url),body:{
-        "id" : _id,
-    });
-      print("_GetContactData query_");
-      print("1: "+response.body);
-      //User user = User.fromJson(json.decode(response.body)) ;
-      String data = response.body; //response.body returns String
-      contact = data.split(','); // data = "data1,data2,data3"
-      print("2: "+contact.toString());
+      bool isSession = await sessions.verificationSession("id");
+      if (isSession)
+      {
+        var iduser = await sessions.getSessionValue("id");
+        sessionId = int.parse(iduser);
+        print("sessionId: "+sessionId.toString());
+      }
     }catch(e)
     {
-      return "NoResponse";
+      print("  2 msgError: "+e.toString());
     }
-    return contact;
-  }
-  */
-  // 2 INSERT query: register emergency contact
-  Future InsertEmergencyContact(
-      String _id, String _nameContact, String _number) async {
-    var url = db + "insertEmergencyContact.php";
-    String data = "";
-    try {
-      var response = await http.post(Uri.parse(url), body: {
-        "id": _id,
-        "nameContact": _nameContact,
-        "number": _number,
-      });
-      print("_InsertEmergencyContact query_");
-      print("1: " + response.body);
-      //User user = User.fromJson(json.decode(response.body)) ;
-      data = response.body;
-      print("2: " + data);
-    } catch (e) {
-      //ShowCustomToast("No se pudo conectar con el servidor...", Colors.red);
-    }
-
-    return data;
   }
 
-  // 3 UPDATE query: updates data user
-  /*
-  Future UpdateUserData(String _id, String _fullname,String _cellphone,String _email) async
+ void onPressedBtnAgregarContacto(BuildContext context) async
+ {
+   if(contactNumber_Controller.text.length !=8)
+   {
+      ShowCustomToast("Ingrese un número de 8 dígitos", Colors.red);
+   }
+   else
+   {
+     print("LA SESION ID: "+sessionId.toString());
+     bool result = await emergencyContactService.insertEmergencyContact(
+       new EmergencyContact.insert(
+         contactName_Controller.text, 
+         contactNumber_Controller.text,
+         sessionId
+         ));
+
+     if(result) {ShowCustomToast("Se han guardado los cambios.", Colors.green);Navigator.pop(context);}
+     else {ShowCustomToast("Error, inténtelo de nuevo mas tarde...", Colors.red);}
+
+   }
+
+ }
+
+  void onPressedBtnEditarContact(BuildContext context, idEmergencyContact) async
   {
-    var url = db+"updateUser.php";
-    var response = await http.post(Uri.parse(url),body:{
-      "id" : _id,
-      "fullname" : _fullname,
-      "cellphone" : _cellphone,
-      "email" : _email,
-    });
-    print("_UpdateUserData query_");
-    print("1: "+response.body);
-    //User user = User.fromJson(json.decode(response.body)) ;
-    String data = response.body;
-    print("2: "+data);
-    return data;
-  }
-  */
-  // 4 SOFT DELETE: realize a UPDATE, status = 0
-  //no usado aun
-  Future DeleteHardContact(String _id) async {
-    var url = db + "deleteEmergencyContact.php";
-    var response = await http.post(Uri.parse(url), body: {
-      "id": _id,
-    });
-    print("_DeleteSoftUser query_");
-    print("1: " + response.body);
-    //User user = User.fromJson(json.decode(response.body)) ;
-    String data = response.body;
-    print("2: " + data);
-    return data;
-  }
+    bool result =await emergencyContactService.updateEmergencyContact(
+      new EmergencyContact.update(
+        idEmergencyContact,
+        contactName_Controller.text,
+        contactNumber_Controller.text));
 
-  // UI FUNCTIONS
-
-  void InsertContact_Function(BuildContext context) async {
-    String result = await InsertEmergencyContact(
-        idUser, contactName_Controller.text, contactNumber_Controller.text);
-
-    if (result == "Success") {
-      ShowCustomToast("Se han guardado los cambios.", Colors.green);
-      Navigator.pop(context);
-    } else {
-      ShowCustomToast("Error, inténtelo de nuevo mas tarde...", Colors.red);
-    }
-  }
-/*
-  void EditContact_Function() async
-  {
-    String result =await UpdateUserData(idUser, nombreCompleto_Controller.text, cellphone_Controller.text, email_Controller.text);
-
-    if(result=="Success") {ShowCustomToast("Se han guardado los cambios.", Colors.green);}
+    if(result) {ShowCustomToast("Se han guardado los cambios.", Colors.green);Navigator.pop(context);}
     else {ShowCustomToast("Error, inténtelo de nuevo mas tarde...", Colors.red);}
   }
-*/
-/*
-  void DeleteContact_Function() async
+
+  void ShowCustomToast(String myText, Color myColor)
   {
-    String result =await DeleteHardContact(id);
-
-    if(result=="Success")
-    {
-      ShowCustomToast("Se ha desactivado la cuenta.", Colors.green);
-    }
-    else {ShowCustomToast("Error, inténtelo de nuevo mas tarde...", Colors.red);}
-  }
-  */
-
-  void ShowCustomToast(String myText, Color myColor) {
-    Fluttertoast.showToast(
+        Fluttertoast.showToast(
         msg: myText,
         toastLength: Toast.LENGTH_SHORT,
         gravity: ToastGravity.CENTER,
         backgroundColor: myColor,
         textColor: Colors.white);
   }
+  
 }
--- lib/pages/emergencyContact/contact_form_page.dart
+++ lib/pages/emergencyContact/contact_form_page.dart
@@ -1,101 +1,122 @@
 import 'package:flutter/material.dart';
+import 'package:taxi_segurito_app/models/emergencyContact.dart';
 import 'package:taxi_segurito_app/pages/emergencyContact/contact_form_functionality.dart';
 
-class FormContact_Page extends StatefulWidget {
-  _FormState createState() => _FormState();
+class FormContact_Page extends StatefulWidget 
+{
+  EmergencyContact _dataContact = new EmergencyContact.delete(0);
+
+  FormContact_Page.insert();
+  FormContact_Page.update(this._dataContact); //Aux para pasar datos de contacto
+
+  _FormState createState() => _FormState(_dataContact);
+  
 }
 
 class _FormState extends State<FormContact_Page> {
-  FormContact_Functionality formContact_Functionality =
-      new FormContact_Functionality();
-  var aux; //Para esperar la respuesta del futureBuilder
-  var isSession;
+
+  EmergencyContact _dataContact = new EmergencyContact.delete(0);
+  _FormState(this._dataContact); //Aux para pasar datos de contacto
+ 
+  FormContact_Functionality formContact_functionality = new FormContact_Functionality();
+
   @override
-  void initState() {
-    LoadData_Function();
+  initState()
+  {
+    super.initState();
+    loadData();
   }
 
-  void LoadData_Function() async {
-    isSession =
-        await formContact_Functionality.CheckID(); //Revisa si hay sesion,
-    print("isSession: " + isSession.toString());
-    if (isSession) {
-      /*
-        List<dynamic> data = await formContact_Functionality.GetContactData();
-        print("2: "+data.toString());
-        if(data.toString()=="NoResponse")
-        {
-          formContact_Functionality.ShowCustomToast("Error al conectar con la base de datos.", Colors.red);
-          return [];
-        }
-        else{
-          //contacts = dataSet1;
-          return data;
-        } */
+  void loadData() async
+  {
+    await formContact_functionality.CheckSession();
+    if(_dataContact.idEmergencyContact!=0)
+    {
+    setState(() {
+      formContact_functionality.contactName_Controller.text = _dataContact.nameContact;
+      formContact_functionality.contactNumber_Controller.text = _dataContact.number;
+    });
+
     }
-  }
 
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        title: Text("Registrar contacto"),
-        backgroundColor: Colors.yellow,
-        foregroundColor: Colors.black,
-        leading: Builder(
-          builder: (BuildContext context) {
-            return IconButton(
-                onPressed: () => Navigator.pop(context),
-                icon: Icon(
-                  Icons.arrow_back,
-                  color: Colors.black,
-                ));
-          },
+          centerTitle: true,
+          title: (_dataContact.idEmergencyContact==0) ?Text("Registrar contacto"):Text("Editar contacto"),
+          backgroundColor: Colors.yellow,
+          foregroundColor: Colors.black,
+          leading: Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                  onPressed: () => Navigator.pop(context),
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: Colors.black,
+                  ));
+            },
+          ),
         ),
-      ),
       body: SingleChildScrollView(
-        child: Container(
-          margin:
-              EdgeInsets.only(left: 20.0, right: 20.0, top: 40.0, bottom: 20.0),
-          child: Column(
+        child : Container(
+        margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 40.0, bottom: 20.0),
+        child:Column(
             children: <Widget>[
               //Inputs
               TextField(
-                controller: formContact_Functionality.contactName_Controller,
+                controller: formContact_functionality.contactName_Controller,
                 decoration: InputDecoration(
                   hintText: "  Apodo del contacto",
                   icon: const Icon(
-                    Icons.person_outline,
-                    size: 35,
-                  ),
+                            Icons.person_outline,
+                            size: 35,
+                          ),
                 ),
               ),
               Container(
-                margin: EdgeInsets.only(top: 20, bottom: 20),
-                child: TextField(
-                  controller:
-                      formContact_Functionality.contactNumber_Controller,
-                  decoration: InputDecoration(
-                    hintText: "  Numero del contacto",
-                    icon: const Icon(
-                      Icons.phone,
-                      size: 35,
-                    ),
-                  ),
+                margin: EdgeInsets.only(top: 20,bottom: 20),
+                child :
+              TextField(
+                controller: formContact_functionality.contactNumber_Controller,
+                decoration: InputDecoration(
+                  hintText: "  Numero del contacto",
+                  icon: const Icon(
+                            Icons.phone,
+                            size: 35,
+                          ),
                 ),
+              ),
               ),
               Padding(padding: EdgeInsets.only(top: 30.0)),
               ElevatedButton(
-                  child:
-                      Text("Agregar contacto", style: TextStyle(fontSize: 20)),
-                  onPressed: () {
-                    formContact_Functionality.InsertContact_Function(context);
-                  }),
+                child: (_dataContact.idEmergencyContact==0) ?Text(
+                  "Agregar contacto",
+                  style: TextStyle(fontSize: 20)
+                  ):Text(
+                  "Editar contacto",
+                  style: TextStyle(fontSize: 20)
+                  )
+                  ,
+                onPressed: (){
+                  if(_dataContact.idEmergencyContact==0) // Si estamos haciendo un INSERT
+                  {
+                    formContact_functionality.onPressedBtnAgregarContacto(context);
+                  }
+                  else                  // Si estamos haciendo un UPDATE
+                  {
+                    formContact_functionality.onPressedBtnEditarContact(context,_dataContact.idEmergencyContact);
+                  }
+                },
+              ),
+
             ],
           ),
+         ),
         ),
-      ),
-    );
+
+
+      );
   }
 }
