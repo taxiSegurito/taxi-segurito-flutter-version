e1918b30d0c37da71b85a4d12cadda0b6e7724df
LitosCar19 <carlitos.sebu@gmail.com>
2021-11-14 20:40:49 -0400

    View coments of my car

--- lib/pages/forgetObjects/forgetObjectMain.dart
+++ lib/pages/forgetObjects/forgetObjectMain.dart
@@ -7,9 +7,7 @@ import 'package:taxi_segurito_app/components/inputs/CustomTextField.dart';
 
 import 'package:taxi_segurito_app/components/inputs/CustomTextFieldArea.dart';
 
-void main() {
-  runApp(MyApp());
-}
+
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
