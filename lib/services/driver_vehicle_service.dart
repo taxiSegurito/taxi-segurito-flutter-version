b108c4a153a13e2416ac783c3ae3bfa2e47f6998
Sofia Toro Chambi <sofie.torch@outlook.com>
2021-11-18 02:50:58 -0400

    add get driver vehicle service by vehicle id

--- lib/services/driver_vehicle_service.dart
+++ lib/services/driver_vehicle_service.dart
@@ -45,4 +45,24 @@ class DriverVehicleService {
     }
     return null;
   }
+
+  Future<Map<String, dynamic>?> getAssignationByVehicleId(int vehicle) async {
+    final queryParams = {'vehicleId': vehicle.toString()};
+    final endpoint = Uri.http(
+      Server.host,
+      '${Server.baseEndpoint}/driver_has_vehicle/driverhv_controller.php',
+      queryParams,
+    );
+
+    Response response = await http.get(endpoint);
+    if (response.statusCode == 200) {
+      final body = jsonDecode(response.body);
+      return {
+        'idDriverHasVehicle': body['idDriverHasVehicle'] as int,
+        'driver': Driver.fromJson(body['driver']),
+        'vehicle': Vehicle.fromJson(body['vehicle'])
+      };
+    }
+    return null;
+  }
 }
