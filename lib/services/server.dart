37f0a49a3677a53912111eac1aa0809488659b25
Sofia Toro Chambi <sofie.torch@outlook.com>
2021-11-18 15:02:19 -0400

    final server host changed, aws!

--- lib/services/server.dart
+++ lib/services/server.dart
@@ -4,7 +4,7 @@ class Server {
   //     "http://10.0.2.2:8070/PHP_TAXISEGURITIO/taxi-segurito-backend/app/api/";
 
   static const protocol = "http://";
-  static const host = "07d9-186-2-7-156.ngrok.io";
+  static const host = "ec2-34-207-65-236.compute-1.amazonaws.com:81";
   // static const host = "10.0.3.2:8086";
   static const baseEndpoint = "/app/api";
   /* static const host = "192.168.0.18"; //static const host = "637e-161-138-17-0.ngrok.io"; 
